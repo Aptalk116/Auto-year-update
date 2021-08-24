888084916741da2d4f0ba59df9a8e083d8e1ebd7
Aptalk116 <89452474+Aptalk116@users.noreply.github.com>
2021-08-24 23:06:42 +0530

    script

--- Main.js
+++ Main.js
@@ -1,6 +1,5 @@
-<script type="text/javascript">
+
 /* auto Year update */
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("autoYear").innerHTML = new Date().getFullYear();
 });
-</script>
